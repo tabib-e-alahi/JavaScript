a928b34510d336271edda9841792962e60c041cd
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-26 11:53:13 +0600

    js info added

---
+++ Introduction.js
@@ -0,0 +1,12 @@
+//JavaScript
+/*
+-> Is high-level annd Interpreted
+-> Dynamically update content
+-> Control multimedia
+-> Animate images
+-> Pretty much everything else
+-> JavaScript to program the behavior of web pages
+
+**All the scripting languages are programming languages. It is a type of programming language in which there is no compilation and the execution takes place line by line. Generally, a programming language would be first compiled and then executed, but in a scripting language, the program will be executed, and there is no compilation step required.
+
+*/
