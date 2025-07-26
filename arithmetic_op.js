cd4df7b3f75a5bbfce0d78ae2e77d0ba624a85d7
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-26 11:46:49 +0600

    arithmetic operations added

---
+++ arithmetic_op.js
@@ -0,0 +1,27 @@
+var price  = 50
+
+//Addition------------------------
+price += 20 // shorthand for price = price + 20
+console.log("After adding 20: ", price);
+
+//Subtraction---------------------
+price -= 30 // shorthand for price = price - 30
+console.log("After subtracting 30: ", price);
+
+//Multiplying---------------------
+price *= 20 // shorthand for price = price * 20
+console.log("After multiplying by 20: ", price);
+
+
+//Division---------------------
+price /= 5 // shorthand for price = price / 20
+console.log("After dividing by 5: ", price);
+
+// Modulus---------------------
+var rem = price % 13 
+console.log("Remainder: ", rem);
+
+//Exponentiation------------
+
+console.log(4**2)
+
--- numbers.js
+++ numbers.js
@@ -5,6 +5,7 @@ var c = '34'
 // console.log(typeof b)
 // console.log(typeof c)
 
+
 //convert numerical string to numeric
 var numeric_c = parseInt(c)
 // console.log(numeric_c, typeof numeric_c)
@@ -17,14 +18,17 @@ var convert_name = parseInt(name)
 
 var price = '22.6'
 var price_int = parseInt(price)
-console.log(price_int)
+// console.log(price_int)
 
 var price_float = parseFloat(price)
 
-console.log(price_float)
+// console.log(price_float)
 
 var a = 0.1
 var b = 0.2
 var c = a + b
-console.log(c)
-console.log(c.toFixed(2))
+// console.log(c)
+// console.log(c.toFixed(2))
+
+console.log(50/0) //   Infinity
+console.log(-50/0) // -Infinity
