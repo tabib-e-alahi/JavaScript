4e4b488fc64359c22c629402912de31cf0087d7b
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-26 10:49:55 +0600

    varibales types added

---
+++ variable.js
@@ -0,0 +1,15 @@
+//integer
+var age = 25
+console.log(age, typeof age)
+
+//string
+var name = "Tabib E Alahi"
+console.log(name, typeof name)
+
+//Boolean
+var is_on = true
+var is_off = false
+console.log(is_on, is_off, typeof is_on)
+
+var price = 45.5
+console.log(typeof price)
