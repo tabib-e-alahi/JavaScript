e36e1ec83e50f2153e3a254d57a81c1f93e0b026
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-27 22:34:15 +0600

    js conditional created

---
+++ js_conditionals/comparison.js
@@ -0,0 +1,16 @@
+/*
+    COMPARISON
+
+ -> greater than '>'   
+ -> less than '<'
+ -> greaer than or equal to '>='   
+ -> less than or equal to '<='
+ -> equal to '==' 
+ -> not equal to '!='
+ 
+ -----------------
+
+ -> and: &&
+ -> or : ||
+
+*/
