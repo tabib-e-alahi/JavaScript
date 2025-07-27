c141ba0e8fc9c643f52a3f1e89f5d163d0a84c58
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-27 23:24:19 +0600

    ternary operator added

---
+++ js_conditionals/ternary.js
@@ -0,0 +1,9 @@
+//simpel syntax
+// variablename = (condition) ? value1 : value2 
+const age = 19
+
+const canVote = (age >= 18) ? true : false
+
+console.log(canVote)
+
+console.log(age >= 18 ? "You can vote" : "You can not vote")
