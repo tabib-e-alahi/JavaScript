8a9585044433f7c94f401606da650d1755bf405a
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-28 21:11:12 +0600

    index array added

--- Fundamental_concepts_Array/array.js
+++ Fundamental_concepts_Array/array.js
@@ -1,2 +1,22 @@
 const numbers = [1, 2, 45, 67, 76, 45];
-console.log(numbers)
+// console.log(numbers)
+
+/*
+
+An Array is an object type designed for storing data collections.
+
+Key characteristics of JavaScript arrays are:
+
+-> Elements: An array is a list of values, known as elements.
+
+-> Ordered: Array elements are ordered based on their index.
+
+-> Zero indexed: The first element is at index 0, the second at index 1, and so on.
+
+-> Dynamic size: Arrays can grow or shrink as elements are added or removed.
+
+-> Heterogeneous: Arrays can store elements of different data types (numbers, strings, objects and other arrays).
+
+*/
+
+console.log(numbers.length)
---
+++ Fundamental_concepts_Array/index.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 45, 67, 76, 45];
+// array are O-index based, first element index is 0
+
+//get element id by index
+console.log(numbers[0])
+console.log(numbers[1])
+console.log(numbers[2])
+console.log(numbers[3])
+
+//set or update element val using index
+numbers[3] = 89 // chnaging 67 to 89
+
+console.log(numbers)
