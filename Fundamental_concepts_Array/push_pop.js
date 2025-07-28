ec34c041bb3e0fd8505045c68f64a66475380b1d
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-28 21:17:00 +0600

    array push pop added

---
+++ Fundamental_concepts_Array/push_pop.js
@@ -0,0 +1,9 @@
+const ages = []
+const numbers = [17, 29, 45, 67, 76, 45];
+
+numbers.push(56) // adding 56 to the end
+numbers.push(43)
+numbers.push(34, 100, 98)
+console.log(numbers)
+numbers.pop() // removing the last element
+console.log(numbers)
