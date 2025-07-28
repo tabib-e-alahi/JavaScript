78e08fb15c8b9bb0a22a81f03c9c6f6960a4dda1
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-28 21:20:13 +0600

    array shift unshift added

---
+++ Fundamental_concepts_Array/shift_unshift.js
@@ -0,0 +1,7 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits)
+fruits.shift() //Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+console.log(fruits)
+
+fruits.unshift("Grape") //Elements to insert at the start of the array.
+console.log(fruits)
