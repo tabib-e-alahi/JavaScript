0335dc724bbc9b2bfc8728181cfe63f9f5b1f248
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-28 21:30:37 +0600

    array isArray added

--- Fundamental_concepts_Array/isArray.js
+++ Fundamental_concepts_Array/isArray.js
@@ -1,1 +1,10 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango", "Grape", "Cherry"];
+
+const age = 25
+
+const numbers = []
+
+// checking array or is not array
+console.log(Array.isArray(fruits)) // true
+console.log(Array.isArray(age)) // false
+console.log(Array.isArray(numbers)) // true
