80fb62451fb664df2a9ddb16468e5927a7a9978c
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-30 11:07:55 +0600

    Fundamental concept of loops: for loop concept added

---
+++ Fundamental_concept_Loops/for_loop.js
@@ -0,0 +1,23 @@
+/*
+------- When to use:
+
+-> When you know the number of iterations or need to iterate over a range of numbers or indices.
+-> It's best used when you have a fixed start, end, and step value, like iterating through arrays, strings, or performing repeated tasks a set number of times.
+
+*/
+
+// for(let num = 0; num < 20; num++)
+// {
+//     console.log(num);
+// }
+
+for(let i = 1; i <= 10; i++)
+{
+    if(i % 2 == 0)
+    {
+        console.log(i ,"is even");
+    }
+    else{
+        console.log(i ,"is odd");
+    }
+}
