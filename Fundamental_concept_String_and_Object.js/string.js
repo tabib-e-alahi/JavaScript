f3f02d8cc86fb18b76d5161a84128409c66ecfde
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-30 13:09:28 +0600

    Fundamental concept of string and object: string vs array added

--- Fundamental_concept_String_and_Object.js/string.js
+++ Fundamental_concept_String_and_Object.js/string.js
@@ -3,7 +3,16 @@ const road = `Dada More`;
 const post_office = `5600`;
 const thana = new String('Kurigram');
 
-console.log(typeof home); // string
-console.log(typeof road); // string
-console.log(typeof post_office); // string
-console.log(typeof thana); // object
+// console.log(typeof home); // string
+// console.log(typeof road); // string
+// console.log(typeof post_office); // string
+// console.log(typeof thana); // object
+
+// console.log(thana); // [String: 'Kurigram']
+
+console.log(home.length);
+console.log(home[0]);
+//string indexing behave like array
+//But string is immutable , you can not change string with index value
+home[3] = 'P'; // It won;t change anything
+console.log(home);
