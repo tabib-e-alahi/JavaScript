b4685c6a99b7d787e92b9ed0e45b874096c46204
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-30 12:49:33 +0600

    Fundamental concept of loops added

--- Fundamental_concept_Loops/for-in_loop.js
+++ Fundamental_concept_Loops/for-in_loop.js
@@ -2,15 +2,13 @@ const numbers = [45, 4, 9, 16, 25];
 
 
 for (let x in numbers) {
-  console.log(numbers[x])
+    console.log(numbers[x])
 }
 
 /*
-When to use:
+========= When to use:
 
-When iterating over the properties (keys) of an object.
-
-It’s best for working with objects to loop through keys and their corresponding values.
-
-It can also be used for arrays, but it’s less efficient than other methods (e.g., for or for...of).
+-> When iterating over the properties (keys) of an object.
+-> It’s best for working with objects to loop through keys and their corresponding values.
+-> It can also be used for arrays, but it’s less efficient than other methods (e.g., for or for...of).
 */
