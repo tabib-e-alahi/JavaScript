a256513aedfb0a81b8e0da68f61ba94e75f0d882
tabib-e-alahi <etabib.alahi@gmail.com>
2025-07-30 13:03:31 +0600

    Fundamental concept of string and object: folder added

---
+++ Fundamental_concept_String_and_Object.js/string.js
@@ -0,0 +1,9 @@
+const home = "Ma Villa";
+const road = `Dada More`;
+const post_office = `5600`;
+const thana = new String('Kurigram');
+
+console.log(typeof home); // string
+console.log(typeof road); // string
+console.log(typeof post_office); // string
+console.log(typeof thana); // object
