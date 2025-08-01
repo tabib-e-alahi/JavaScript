ba571a0d3be22259eefb65998a7ede950873c85c
tabib-e-alahi <etabib.alahi@gmail.com>
2025-08-01 10:11:59 +0600

    functions intro added

---
+++ Fundamental_concept_Functions/function.js
@@ -0,0 +1,21 @@
+/**
+ ** What are Functions?
+    => Functions are fundamental building blocks in all programming.
+    => Functions enable better code organization, modularity, and efficiency.
+    => Functions are reusable block of code designed to perform a particular task.
+    => Functions execute when they are "called" or "invoked".
+
+ ** Why Functions?
+    => With functions you can reuse code
+    => You can write code that can be used many times.
+    => You can use the same code with different arguments, to produce different results.
+ */
+
+const number = 67
+
+function print_value(num)
+{
+    console.log(num);
+}
+
+print_value(number)
