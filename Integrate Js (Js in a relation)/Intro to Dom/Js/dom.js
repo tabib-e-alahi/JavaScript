30893b3f857baed1080bfd74914cff5a0716c471
tabib-e-alahi <etabib.alahi@gmail.com>
2025-08-02 13:29:58 +0600

    Integrate Js: dom definition added

--- Integrate Js (Js in a relation)/Intro to Dom/Js/dom.js
+++ Integrate Js (Js in a relation)/Intro to Dom/Js/dom.js
@@ -1,1 +1,20 @@
+/*
+====# What is DOM?
+=> The Document Object Model (DOM) is a programming interface for web documents. It represents the page so that programs can change the document structure, style, and content. The DOM represents the document as nodes and objects; that way, programming languages can interact with the page.
+
+
+###### What Javascript can do with DOM?
+
+With the object model, JavaScript gets all the power it needs to create dynamic HTML:
+    => JavaScript can change all the HTML elements in the page
+    => JavaScript can change all the HTML attributes in the page
+    => JavaScript can change all the CSS styles in the page
+    => JavaScript can remove existing HTML elements and attributes
+    => JavaScript can add new HTML elements and attributes
+    => JavaScript can react to all existing HTML events in the page
+    => JavaScript can create new HTML events in the page
+
+*/
+
 console.log("Js connected to HTML.")
+console.log(document.body)
