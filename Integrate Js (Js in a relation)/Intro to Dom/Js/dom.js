ed8ce1d6e0ce37ce831ef753ea0818e2834362fe
tabib-e-alahi <etabib.alahi@gmail.com>
2025-08-03 13:09:07 +0600

    internal js moved to external file

--- Integrate Js (Js in a relation)/Intro to Dom/Js/dom.js
+++ Integrate Js (Js in a relation)/Intro to Dom/Js/dom.js
@@ -20,9 +20,39 @@ console.log("Js connected to HTML.")
 // console.log(document.body)
 
 const li_collection = document.getElementsByTagName('li')
-console.log(li_collection)
+// console.log(li_collection)
 
-for(const li of li_collection)
-{
+for (const li of li_collection) {
     // console.log(li.innerText)
 }
+
+const food_list = document.getElementsByClassName("food");
+console.log(food_list[0]);
+const food_title = document.getElementById("food-title");
+console.log(food_title.innerText);
+//change the food title
+food_title.innerText = "Food title changed from source code.";
+
+const foreign_class = document.getElementsByClassName("foreign");
+for (const for_class of foreign_class) {
+    console.log(for_class.innerText);
+}
+
+const foreign_class_with_querySelector = document.querySelectorAll(".foreign");
+console.log(foreign_class_with_querySelector)
+
+//getElementsByClassName vs querySelectorAll
+/*
+1. Return:
+=> getElementsByClassName return An HTMLCollection (live collection).
+=> querySelectorAll return A NodeList (static collection).
+
+2. Access:
+=> HTMLCollection works like an array but not truly an array. Use items[0], but not forEach directly (convert first: Array.from(items)).
+=> NodeList works more like an array. You can use forEach directly.
+
+3. Live vs static collection:
+=> Live: updates automatically if the DOM changes.
+Example: If you add a new element with the same class later, it appears in the collection.
+
+*/
