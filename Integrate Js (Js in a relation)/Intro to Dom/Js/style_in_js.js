7fc0f7ba9a9ab955d54d6f54841a606adcdf654d
tabib-e-alahi <etabib.alahi@gmail.com>
2025-08-03 17:58:12 +0600

    dom style properties by id

--- Integrate Js (Js in a relation)/Intro to Dom/Js/style_in_js.js
+++ Integrate Js (Js in a relation)/Intro to Dom/Js/style_in_js.js
@@ -8,4 +8,13 @@ for(const section of sections)
     section.style.padding = "10px";
     section.style.marginBottom = "20px";
     section.style.borderRadius = "10px";
+    section.style.backgroundColor = "lightgray"
 }
+
+//If want to style a specific element, then the best practice is use "id"
+
+const playerContainer = document.getElementById("player-list");
+playerContainer.style.fontSize = "24px"; 
+playerContainer.style.fontWeight = "800";
+playerContainer.style.color = "steelblue";
+ 
