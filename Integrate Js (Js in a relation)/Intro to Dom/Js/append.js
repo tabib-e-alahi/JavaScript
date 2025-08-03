e4ab2029a286eb17b02a333b8b1d43ebd7071836
tabib-e-alahi <etabib.alahi@gmail.com>
2025-08-03 18:51:28 +0600

    a new list item created added to ul

--- Integrate Js (Js in a relation)/Intro to Dom/Js/append.js
+++ Integrate Js (Js in a relation)/Intro to Dom/Js/append.js
@@ -1,1 +1,9 @@
 console.log("append.js is running.")
+
+//adding a new list item in the address section
+
+const container = document.getElementById("list_container");
+
+const new_li = document.createElement("li");
+new_li.innerText = "Upazilla: Kurigram Sadar (added with JS)";
+container.appendChild(new_li);
